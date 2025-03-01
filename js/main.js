1bf11e97ad98581f55678d60c66ba3a22892ce0e
Alfredo Cruz <acruz12131@gmail.com>
2025-03-01 15:24:43 -0600

    invitados

--- js/main.js
+++ js/main.js
@@ -6,11 +6,28 @@
         setTimeout(function () {
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
+				
+				const queryString = window.location.search;
+				const urlParams = new URLSearchParams(queryString);
+				let invitados = atob(urlParams.get('np'));
+				if(invitados){
+					switch(invitados){
+						case 3:
+						case 4: 
+						case 5: 
+						case 6:
+						case 7:
+							break;
+						default:
+							window.location.href = "notfound.html";
+					}
+				}
             }
         }, 1500);
     };
     spinner();
     
+	
     
     // Initiate the wowjs
     new WOW().init();
