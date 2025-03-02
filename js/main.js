e59f5f3bf5bec0cdb77ad097b1cdf6d848ac46a3
Alfredo Cruz <acruz12131@gmail.com>
2025-03-01 19:46:03 -0600

    fix: invitados

--- js/main.js
+++ js/main.js
@@ -4,25 +4,38 @@
     // Spinner
     var spinner = function () {
         setTimeout(function () {
+			
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
 				
-				const queryString = window.location.search;
-				const urlParams = new URLSearchParams(queryString);
-				let invitados = atob(urlParams.get('np'));
-				if(invitados){
-					switch(invitados){
-						case 3:
-						case 4: 
-						case 5: 
-						case 6:
-						case 7:
-							break;
-						default:
-							window.location.href = "notfound.html";
+				try {
+
+					const queryString = window.location.search;
+					const urlParams = new URLSearchParams(queryString);
+					let invitados = atob(urlParams.get('np'));
+					
+					if(invitados){
+						
+						switch(invitados){
+							case "3":
+							case "4": 
+							case "5": 
+							case "6":
+							case "7":
+								$("#pases").text(invitados);
+							default:
+								window.location.href = "notfound.html";
+						}
+					} else {
+						window.location.href = "notfound.html";
 					}
+				
+				} catch(err) {
+				  window.location.href = "notfound.html";
 				}
+					
             }
+			
         }, 1500);
     };
     spinner();
